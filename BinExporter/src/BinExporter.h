ff5e4bb81ff79a8c557014dbc5b2b43af647e9f8
Thomas Kroes <t.kroes@lumc.nl>
2022-08-29 10:00:00 +0200

    Adhere to adjusted core API

--- BinExporter/src/BinExporter.h
+++ BinExporter/src/BinExporter.h
@@ -129,8 +129,12 @@ public:
     BinExporterFactory(void) {}
     ~BinExporterFactory(void) override {}
 
-    /** Returns the plugin icon */
-    QIcon getIcon() const override;
+    /**
+     * Get plugin icon
+     * @param color Icon color for flat (font) icons
+     * @return Icon
+     */
+    QIcon getIcon(const QColor& color = Qt::black) const override;
 
     WriterPlugin* produce() override;
 
