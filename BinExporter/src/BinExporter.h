c17bedf18a23c1683c07b37faf30338963850440
Thomas Kroes <tkroes81@gmail.com>
2025-02-27 14:46:44 +0100

    Remove redundant override

--- BinExporter/src/BinExporter.h
+++ BinExporter/src/BinExporter.h
@@ -128,13 +128,6 @@ public:
     BinExporterFactory(void) {}
     ~BinExporterFactory(void) override {}
 
-    /**
-     * Get plugin icon
-     * @param color Icon color for flat (font) icons
-     * @return Icon
-     */
-    QIcon getIcon(const QColor& color = Qt::black) const override;
-
     WriterPlugin* produce() override;
 
     mv::DataTypes supportedDataTypes() const override;
