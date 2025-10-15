d74f8011609e1d27854cde35a931d4b5fe74fa32
Thomas Kroes <t.kroes@lumc.nl>
2025-10-15 10:31:58 +0200

    Update PDB file copy destination in conanfile.py
    Change the destination for copying PDB files from pdb_dest to 'package'.

--- conanfile.py
+++ conanfile.py
@@ -151,7 +151,7 @@ class BinIOPluginConan(ConanFile):
             pdb_files = pdb_files = [p for p in pathlib.Path(self.build_folder).rglob('*') if p.is_file() and p.suffix.lower() == '.pdb']
             print("PDB(s): ", pdb_files)
             for pfile in pdb_files:
-                shutil.copy(pfile, pdb_dest)
+                shutil.copy(pfile, "package")
                 
         self.copy(pattern="*", src=package_dir)
 
